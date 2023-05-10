77c12b492405db58684b781e26f66c0606094d0c
Gafar Yahya <hargarx@gmail.com>
2023-05-10 20:22:45 +0300

    adding app cache const

---
+++ lib/src/core/constants/app_cache_consts.dart
@@ -0,0 +1,7 @@
+class AppCacheConsts {
+  const AppCacheConsts._();
+
+  static const Duration maxAgeDuration = Duration(minutes: 30);
+  static const Duration maxStaleDuration = Duration(minutes: 60);
+
+}
