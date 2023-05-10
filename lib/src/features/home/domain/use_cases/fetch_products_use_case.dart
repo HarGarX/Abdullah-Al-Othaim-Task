db2801de98a68b7ed3503f726b6ac83e8805d929
Gafar Yahya <hargarx@gmail.com>
2023-05-10 20:20:30 +0300

    use case

---
+++ lib/src/features/home/domain/use_cases/fetch_products_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:abdullah_al_othaim_task/src/core/errors/failures.dart';
+import 'package:abdullah_al_othaim_task/src/core/use_cases/usecase.dart';
+import 'package:abdullah_al_othaim_task/src/features/home/domain/entites/product_entity.dart';
+import 'package:abdullah_al_othaim_task/src/features/home/domain/repository/home_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchProductsUseCase extends UseCase<List<ProductEntity>, NoParams> {
+  final HomeRepository _homeRepository;
+  FetchProductsUseCase({required HomeRepository homeRepository}) : _homeRepository = homeRepository;
+
+  @override
+  Future<Either<Failure, List<ProductEntity>>> call(NoParams params) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+}
