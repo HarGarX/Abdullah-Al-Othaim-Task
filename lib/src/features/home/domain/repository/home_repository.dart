5ef26070bc71afc6de8cdd7c4a3db98fa1dc02d9
Gafar Yahya <hargarx@gmail.com>
2023-05-10 20:19:37 +0300

    adding home repo contract

---
+++ lib/src/features/home/domain/repository/home_repository.dart
@@ -0,0 +1,7 @@
+import 'package:abdullah_al_othaim_task/src/core/errors/failures.dart';
+import 'package:abdullah_al_othaim_task/src/features/home/domain/entites/product_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepository {
+  Future<Either<Failure, List<ProductEntity>>> fetchProducts();
+}
