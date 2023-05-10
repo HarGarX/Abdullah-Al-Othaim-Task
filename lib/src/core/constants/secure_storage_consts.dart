a4a4d556f7f34c024b25376d578df3d62d6ca67f
Gafar Yahya <hargarx@gmail.com>
2023-05-10 20:17:29 +0300

    adding some of the expected secure storage consts usefaul later

---
+++ lib/src/core/constants/secure_storage_consts.dart
@@ -0,0 +1,7 @@
+class SecureStorageConstants {
+  SecureStorageConstants._();
+
+  static const String ACCOUNT_TOKEN = 'account-token';
+  static const String TOKEN = 'token';
+  static const String DEVICE_INFO = 'device-info';
+}
