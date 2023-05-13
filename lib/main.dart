766286d3b1566fdd1d57d90d04a1fad3a615fa06
Gafar Yahya <hargarx@gmail.com>
2023-05-13 19:19:36 +0300

    add DI , setup and device orientations &

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:abdullah_al_othaim_task/src/app.dart';
+import 'package:abdullah_al_othaim_task/src/core/service_locater.dart' as di;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  runApp(const App());
+  init().then((_) {
+    runApp(const App());
+  });
+}
+
+Future<void> init() async {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  //hide system menu to have Full Splash Screen
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.portraitUp,
+  ]);
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(const Duration(seconds: 3)).then((_) async {
+    await di.setUpLocator();
+    FlutterNativeSplash.remove();
+  });
+  return;
 }
